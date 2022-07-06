3cdebc12880ee99343541a5d26cf9edb9c3f1573
Phil Boyd <Phil.Boyd@csat.com>
2022-07-06 09:40:02 -0400

    updates

--- libs/debian/install-2.sh
+++ libs/debian/install-2.sh
@@ -1,2 +1,11 @@
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source $HOME/.bashrc
+
+nvm install --lts
+
+npm install -g npm && npm --version && \
+    npm install -g yarn && yarn --version && \
+    npm install -g typescript @typescript-eslint/parser vue-eslint-parser && tsc --version && \
+    npm install -g npm-check-updates && ncu --version && \
+    npm install -g newman && newman --version&& \
+    npm install -g @nrwl/cli nx
